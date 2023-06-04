a659711ebf18c3ac1f95c4d9bcc5368ab25b0f5f
giftedtutor <giftedtutorr@gmail.com>
2023-06-04 17:23:02 +0500

    validation and pagination for product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers;
 use App\Models\Product;
-
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+
+    public function index(Request $request)
     {
-        $products = Product::all();
-        return response()->json($products);
+        $perPage = $request->input('records', 10); // Default value is 10 if 'records' parameter is not provided
+    
+        $products = Product::paginate($perPage);
+    
+        return response()->json(['Products' => $products]);
+        // return response()->json($products);
     }
+    
 
     public function create()
     {
@@ -22,36 +29,58 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-        // Validate the request data if needed
-
-        $product = new Product;
-        $product->name = $request->name;
-        $product->description = $request->description;
-        $product->price = $request->price;
-        $product->save();
-
-        return response()->json(['message' => 'Product created successfully']);
+        try {
+            // Validate the request data if needed
+    
+            $product = new Product;
+            $product->name = $request->name;
+            $product->description = $request->description;
+            $product->price = $request->price;
+            $product->save();
+    
+            return response()->json(['status' => 'success', 'message' => 'Product created successfully']);
+        } catch (ValidationException $e) {
+            return response()->json(['status' => 'error', 'message' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()], 500);
+        }
     }
+    
     public function edit(Product $product)
     {
         return response()->json($product);
     }
+
     public function update(Request $request, Product $product)
     {
-        // Validate the request data if needed
-
-        $product->name = $request->name;
-        $product->description = $request->description;
-        $product->price = $request->price;
-        $product->save();
-
-        return response()->json(['message' => 'Product updated successfully']);
+        try {
+            // Validate the request data if needed
+    
+            $validatedData = $request->validate([
+                'name' => 'required',
+                'description' => 'required',
+                'price' => 'required|numeric',
+            ]);
+    
+            $product->name = $validatedData['name'];
+            $product->description = $validatedData['description'];
+            $product->price = $validatedData['price'];
+            $product->save();
+    
+            return response()->json(['status' => 'success', 'message' => 'Product updated successfully']);
+        } catch (ValidationException $e) {
+            return response()->json(['status' => 'error', 'message' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()], 500);
+        }
     }
+    
+    
 
     public function destroy(Product $product)
     {
         $product->delete();
 
-        return response()->json(['message' => 'Product deleted successfully']);
+        return response()->json(['status' => 'success', 'message' => 'Product deleted successfully']);
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
 
 class Product extends Model
 {
     protected $fillable = ['name', 'description', 'price'];
-    use HasFactory;
+
+    public function save(array $options = [])
+    {
+        $validator = Validator::make($this->attributes, $this->getValidationRules());
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
+        parent::save($options);
+    }
+
+    protected function getValidationRules()
+    {
+        return [
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ];
+    }
 }
